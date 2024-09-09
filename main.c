7ce80ce04c3672dcba917d632d1e9ca0a72e7fe4
Mohammed Al Morabit <mal-mora@e3r1p13.1337.ma>
2024-09-09 10:11:47 +0100

    mouse handled

--- main.c
+++ main.c
@@ -12,33 +12,6 @@
 
 #include "includes/Cub3d.h"
 
-void read_map(t_window *window)
-{
-    int fd;
-    int i;
-
-    i = 0;
-    fd = open("maps/map", O_RDONLY);
-    if (fd < 0)
-        (perror("Failed to open map file"), exit(EXIT_FAILURE));
-    window->my_map = malloc((MAP_NUM_ROWS + 1) * sizeof(char *));
-    if (!window->my_map)
-        (perror("Failed to read line from map"), exit(EXIT_FAILURE));
-    while (i < MAP_NUM_ROWS)
-    {
-        window->my_map[i] = get_next_line(fd);
-        // printf("%s\n",  window->my_map[i]);
-        if (!window->my_map[i])
-            (perror("Failed to read line fromvbv map"), exit(EXIT_FAILURE));
-        window->my_map[i] = get_next_line(fd);
-        if (!window->my_map[i])
-           break ;
-        i++;
-    }
-    window->my_map[MAP_NUM_ROWS] = NULL;
-    close(fd);
-}
-
 void    re_init_window(t_window *window)
 {
     mlx_delete_image(window->mlx_con, window->img);
@@ -48,13 +21,24 @@ void    re_init_window(t_window *window)
 	mlx_image_to_window(window->mlx_con ,window->img, 0, 0);
 }
 
+void handle_mouse_rotation(t_window *window)
+{
+    int x, y;
+    static int last_x = 0;
+
+    mlx_get_mouse_pos(window->mlx_con, &x, &y);
+    window->player.rotation_angle += (x - last_x) * 0.01;
+    last_x = x;
+}
+
 void    loop_func(void *param)
 {
     t_window *window;
 
     window = (t_window *)param;
     re_init_window(window);
-    update_player(window); // Update player position here instead of in listen_events for better performance
+    update_player(window);
+    handle_mouse_rotation(window);
     render(window);
 }
 
@@ -63,7 +47,6 @@ static void display_window(t_window *window)
     init_window(window);
     init_player(&window->player, window->map);
     init_texture(window);
-    // read_map(window); 
     listen_events(window);
     mlx_loop_hook(window->mlx_con, loop_func, window);
     mlx_loop(window->mlx_con);
