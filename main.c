f2b6a2e0d61a3b243c5de5ebf0d9b2df2eb0c1bc
Doliprane <elfathimohammedamine@gmail.com>
2024-09-11 10:33:39 +0100

    get merge fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: moel-fat <moel-fat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 08:17:40 by moel-fat          #+#    #+#             */
-/*   Updated: 2024/09/11 10:30:05 by moel-fat         ###   ########.fr       */
+/*   Updated: 2024/09/11 10:31:53 by moel-fat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,8 +78,6 @@ void    loop_func(void *param)
     t_window *window;
 
     window = (t_window *)param;
-    update_player(window);
-    re_init_window(window);
     update_player(window);
     handle_mouse_rotation(window);
     render(window);
